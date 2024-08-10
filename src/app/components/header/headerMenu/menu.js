d974cf5c68b5a990a897da1337425f6ee2d087d8
sravankumar-samala <sravankumarsamala20@gmail.com>
2024-08-10 23:08:16 +0530

    Modified header menu

--- src/app/components/header/headerMenu/menu.js
+++ src/app/components/header/headerMenu/menu.js
@@ -1,12 +1,21 @@
+import { useState } from 'react/cjs/react.production.min'
 import { headerMenu } from './menu.module.css'
+import { Search, Heart, ShoppingCart, UserRound } from 'lucide-react'
 
 function Menu() {
+    const [language, setLanguage] = useState('English')
+
     return (
         <div className={headerMenu}>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
+            <button><Search /></button>
+            <button><Heart /></button>
+            <button><ShoppingCart /></button>
+            <button><UserRound /></button>
+            <select onChange={(e) => setLanguage(e.target.value)}>
+                <option value='English'>Eng</option>
+                <option value='Telugu'>Tel</option>
+                <option value='Hindi'>Hnd</option>
+            </select>
         </div>
     )
 }
